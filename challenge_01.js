6ef651a881136749ff227ab59eba9181cba84eb4
Phil <p.pick@kladusch.de>
2021-07-28 14:35:01 +0200

    Concats numbers

--- challenge_01.js
+++ challenge_01.js
@@ -8,3 +8,14 @@ Berechnen Sie die Summe der Zahlen.
 Geben Sie die Summe der Zahlen in die Konsole aus:
 "Die Summe der Zahlen ist: summe"
 */
+console.log("Adder: ");
+
+// Fordert zwei Zahlen an
+let number1 = prompt("Enter the first number: ");
+let number2 = prompt("Enter the second number: ");
+
+// Addiert die beiden Zahlen
+let sum = number1 + number2;
+
+console.log("The sum of both numbers is: " + sum);
+
